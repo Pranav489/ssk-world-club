c93502612de15a58f51498a25a31b432b190a0b5
Pranav489 <pranavsonawane489@gmail.com>
2025-08-07 10:38:17 +0530

    Error Page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom'
 import Layout from './Components/Layout/Layout'
 import Home  from './Pages/Home/Home'
+import PageNotFound from './Pages/ErrorPages/NotFound'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>}/>
+          <Route path='*' element={<PageNotFound/>}/>
       </Route>
     )
   )
